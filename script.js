badcb3cba356db1c1f8190ad1a7fb3292ff1754f
kehindeodukoya <43343191+kehindeodukoya@users.noreply.github.com>
2018-12-20 09:14:52 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".baby").click(function() {
+$(".bro").slideToggle();
+$(".bro").fadeToggle ();  
+});
+$(".boo").click(function() {
+$(".hiya").slideUp();
+$(".hiya").fadeIn();
+});
+$(".evil").click(function() {
+$(".helo").fadeToggle();
+$(".helo").slideDown();
+});
+$(".summer").click(function() {
+$(".push").slideUp();
+$(".push").slideDown();
+});
